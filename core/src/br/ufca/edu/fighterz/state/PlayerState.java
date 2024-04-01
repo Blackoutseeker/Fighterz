a72156835bd174003afa9dd4beaa726fc92175f5
Blackoutseeker <felipeoriginal.2019@gmail.com>
2024-03-31 21:13:59 -0300

    feat(state): create PlayerState class

---
+++ core/src/br/ufca/edu/fighterz/state/PlayerState.java
@@ -0,0 +1,25 @@
+package br.ufca.edu.fighterz.state;
+
+public class PlayerState {
+    public float stateTime = 0f;
+    public float idleTimer = 0f;
+    public float currentStateTime = 0f;
+    public boolean shouldPlayIdleAnimation = false;
+    public boolean isWalkingLeft = false;
+    public boolean isWalkingRight = false;
+    public boolean isCrouching = false;
+    public boolean isFacingRight = true;
+    public boolean isLightPunching = false;
+    public  boolean isStrongPunching = false;
+    public  boolean isLightKicking = false;
+    public  boolean isStrongKicking = false;
+    public boolean isAttacking = false;
+    public boolean isGettingHit = false;
+    public boolean isPlayerInputLocked = false;
+
+    public PlayerState() {}
+
+    public PlayerState(boolean isFacingRight) {
+        this.isFacingRight = isFacingRight;
+    }
+}
