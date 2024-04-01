a0bc4473178bcc1099d46800a43f6d553e88130d
Blackoutseeker <felipeoriginal.2019@gmail.com>
2024-03-31 21:39:50 -0300

    refactor(Character): add states, new animations and collision

--- core/src/br/ufca/edu/fighterz/Character.java
+++ core/src/br/ufca/edu/fighterz/Character.java
@@ -1,9 +1,10 @@
 package br.ufca.edu.fighterz;
 
+import br.ufca.edu.fighterz.state.PlayerState;
 import br.ufca.edu.fighterz.sprites.CharacterAnimation;
+import br.ufca.edu.fighterz.collision.CharacterCollision;
+import br.ufca.edu.fighterz.collision.Hitbox;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -11,141 +12,248 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
-public class Character {
+public final class Character {
     private final Sprite sprite;
     private final CharacterAnimation characterAnimation;
+    private final PlayerState playerState;
     private final Animation<TextureRegion> idleAnimation;
     private final Animation<TextureRegion> crouchAnimation;
     private final Animation<TextureRegion> moveForwardAnimation;
     private final Animation<TextureRegion> moveBackAnimation;
     private final Animation<TextureRegion> autoTauntAnimation;
-    private final Animation<TextureRegion> attackAnimation;
-    private float stateTime;
-    private boolean isWalkingLeft;
-    private boolean isWalkingRight;
-    private boolean isCrouching;
+    private final Animation<TextureRegion> lightPunchAnimation;
+    private final Animation<TextureRegion> strongPunchAnimation;
+    private final Animation<TextureRegion> lightKickAnimation;
+    private final Animation<TextureRegion> strongKickAnimation;
+    private final Animation<TextureRegion> lightHitAnimation;
     private final Vector2 position;
-    private final float scale;
-    private boolean isFacingRight;
-    private float idleTimer = 0f;
-    private boolean shouldPlayIdleAnimation = false;
-    private float currentStateTime = 0f;
-    private  boolean isStrongPunching = false;
-    private boolean isLightPunching = false;
-    private  boolean isLightKicking = false;
-    private  boolean  isStrongKicking = false;
-    private final Rectangle rectangle;
-    private final Rectangle attackRectangle;
+    private final CharacterCollision collision;
     private final InputHandler inputHandler;
+    private final boolean shouldListenInputs;
 
-    public Character(final PlayableCharacter playableCharacter, final float scale, final float x, final float y) {
-        this.scale = scale;
-        characterAnimation = new CharacterAnimation(playableCharacter, scale, 1f / 15f);
+    public Character(final PlayableCharacter playableCharacter, final float scale,
+                     final float x, final float y,
+                     final boolean shouldListenInputs) {
+        playerState = new PlayerState();
+        characterAnimation = new CharacterAnimation(playableCharacter, scale, 1f / 12f);
         sprite = characterAnimation.getSprite();
-        rectangle = new Rectangle();
-        attackRectangle = new Rectangle();
-        stateTime = 0f;
         position = new Vector2(x, y);
-        inputHandler = new InputHandler(isLightPunching, isStrongKicking, isLightKicking, idleTimer, isWalkingLeft, isWalkingRight, isCrouching, isStrongPunching);
+        this.shouldListenInputs = shouldListenInputs;
+        inputHandler = new InputHandler(playerState);
+        collision = new CharacterCollision(playerState, scale, position);
+
         idleAnimation = characterAnimation.getIdleAnimation();
         crouchAnimation = characterAnimation.getCrouchAnimation();
         moveForwardAnimation = characterAnimation.getMoveForwardAnimation();
         moveBackAnimation = characterAnimation.getMoveBackAnimation();
         autoTauntAnimation = characterAnimation.getAutoTauntAnimation();
-        attackAnimation = characterAnimation.getAttackAnimation();
+        lightPunchAnimation = characterAnimation.getLightPunchAnimation();
+        strongPunchAnimation = characterAnimation.getStrongPunchAnimation();
+        lightKickAnimation = characterAnimation.getLightKickAnimation();
+        strongKickAnimation = characterAnimation.getStrongKickAnimation();
+        lightHitAnimation = characterAnimation.getLightHitAnimation();
     }
 
     public Vector2 getPosition() {
         return position;
     }
 
-    public Rectangle getRectangle() {
-        return rectangle;
+    public CharacterCollision getCollision() {
+        return collision;
     }
 
-    public Rectangle getAttackRectangle() {
-        return attackRectangle;
-    }
+    public void update(float deltaTime,
+                       CharacterCollision anotherCharacterCollision, Vector2 anotherCharacterPosition,
+                       Rectangle leftWall, Rectangle rightWall) {
+        playerState.stateTime += deltaTime;
+        if (shouldListenInputs) inputHandler.handleInput(deltaTime);
 
-    public void update(float deltaTime, float stageWidth) {
-        stateTime += deltaTime;
-        isWalkingLeft = inputHandler.getIsWalkingLeft();
-        isWalkingRight = inputHandler.getIsWalkingRight();
-        isCrouching = inputHandler.getIsCrouching();
-        isStrongPunching = inputHandler.getIsStrongPunching();
-        isLightPunching = inputHandler.getIsLightPunching();
-        isStrongKicking = inputHandler.getIsStrongKicking();
-        isLightKicking = inputHandler.getIsLightKicking();
+        if (collision.getHit(anotherCharacterCollision.getAttackRectangle(), deltaTime, anotherCharacterPosition))
+            playerState.isGettingHit = true;
 
-        inputHandler.handleInput(deltaTime, shouldPlayIdleAnimation);
-
-        if (idleTimer >= 7f) {
-            shouldPlayIdleAnimation = true;
-            idleTimer =  0f;
+        if (playerState.idleTimer >= 7f) {
+            playerState.shouldPlayIdleAnimation = true;
+            playerState.idleTimer = 0f;
         }
-        // AUTO_TAUNT
+
         float x = 100f * deltaTime;
-        if (isWalkingLeft) {
-            if (position.x > 0) {
+        if (playerState.isWalkingLeft) {
+            if (collision.checkBodyCollision(anotherCharacterCollision.getBodyRectangle())) {
+                if (!playerState.isFacingRight && !anotherCharacterCollision.checkBodyCollision(leftWall))
+                    anotherCharacterPosition.sub(x, 0);
+            }
+            if (!collision.checkBodyCollision(leftWall))
                 position.sub(x, 0);
-            }
         }
-        else if (isWalkingRight) {
-            if (position.x < stageWidth - 60) {
-                position.add(x, 0);
+        else if (playerState.isWalkingRight) {
+            if (collision.checkBodyCollision(anotherCharacterCollision.getBodyRectangle())) {
+                if (playerState.isFacingRight && !anotherCharacterCollision.checkBodyCollision(rightWall))
+                    anotherCharacterPosition.add(x, 0);
             }
+            if (!collision.checkBodyCollision(rightWall))
+                position.add(x, 0);
         }
 
-        isFacingRight = (position.x < stageWidth / 2f);
+
+        if (collision.checkBodyCollision(anotherCharacterCollision.getBodyRectangle())
+                && !anotherCharacterCollision.checkBodyCollision(leftWall)
+                && !anotherCharacterCollision.checkBodyCollision(rightWall)) {
+            if (playerState.isFacingRight)
+                anotherCharacterPosition.add(2f * deltaTime, 0);
+            else
+                anotherCharacterPosition.sub(2f * deltaTime, 0);
+        }
+
+        playerState.isFacingRight = (position.x < anotherCharacterPosition.x);
     }
 
     public void render(SpriteBatch batch, float deltaTime) {
         TextureRegion currentFrame;
 
-        if (isWalkingLeft) {
-            currentFrame = moveForwardAnimation.getKeyFrame(stateTime, true);
-            if (isFacingRight) currentFrame = moveBackAnimation.getKeyFrame(stateTime, true);
+        if (playerState.isGettingHit) {
+            if (!lightHitAnimation.isAnimationFinished(playerState.currentStateTime)) {
+                currentFrame = lightHitAnimation.getKeyFrame(playerState.currentStateTime, false);
+                playerState.currentStateTime += deltaTime;
+            }
+            else {
+                playerState.isGettingHit = false;
+                currentFrame = idleAnimation.getKeyFrame(playerState.stateTime, true);
+                playerState.currentStateTime = 0f;
+            }
         }
-        else if (isWalkingRight) {
-            currentFrame = moveBackAnimation.getKeyFrame(stateTime, true);
-            if (isFacingRight) currentFrame = moveForwardAnimation.getKeyFrame(stateTime, true);
+
+        else if (playerState.isWalkingLeft) {
+            currentFrame = moveForwardAnimation.getKeyFrame(playerState.stateTime, true);
+            if (playerState.isFacingRight)
+                currentFrame = moveBackAnimation.getKeyFrame(playerState.stateTime, true);
         }
-        else if (isCrouching) {
-            currentFrame = crouchAnimation.getKeyFrame(stateTime, true);
+        else if (playerState.isWalkingRight) {
+            currentFrame = moveBackAnimation.getKeyFrame(playerState.stateTime, true);
+            if (playerState.isFacingRight)
+                currentFrame = moveForwardAnimation.getKeyFrame(playerState.stateTime, true);
+
         }
-        else if (isLightPunching) {
-            if (!attackAnimation.isAnimationFinished(currentStateTime)) {
-                currentFrame = attackAnimation.getKeyFrame(currentStateTime, false);
-                currentStateTime += deltaTime;
-            } else {
-                inputHandler.setIsLightPunching(false);
-                currentFrame = idleAnimation.getKeyFrame(stateTime, true);
-                currentStateTime = 0f;
+        else if (playerState.isCrouching) {
+            currentFrame = crouchAnimation.getKeyFrame(playerState.stateTime, true);
+        }
+        else if (playerState.isLightPunching) {
+            if (!lightPunchAnimation.isAnimationFinished(playerState.currentStateTime)) {
+                currentFrame = lightPunchAnimation.getKeyFrame(playerState.currentStateTime, false);
+                playerState.currentStateTime += deltaTime;
+            }
+            else {
+                playerState.isLightPunching = false;
+                playerState.isAttacking = false;
+                currentFrame = idleAnimation.getKeyFrame(playerState.stateTime, true);
+                playerState.currentStateTime = 0f;
+            }
+        }
+        else if (playerState.isStrongPunching) {
+            if (!strongPunchAnimation.isAnimationFinished(playerState.currentStateTime)) {
+                currentFrame = strongPunchAnimation.getKeyFrame(playerState.currentStateTime, false);
+                playerState.currentStateTime += deltaTime;
+            }
+            else {
+                playerState.isStrongPunching = false;
+                playerState.isAttacking = false;
+                currentFrame = idleAnimation.getKeyFrame(playerState.stateTime, true);
+                playerState.currentStateTime = 0f;
+            }
+        }
+        else if (playerState.isLightKicking) {
+            if (!lightKickAnimation.isAnimationFinished(playerState.currentStateTime)) {
+                currentFrame = lightKickAnimation.getKeyFrame(playerState.currentStateTime, false);
+                playerState.currentStateTime += deltaTime;
+            }
+            else {
+                playerState.isLightKicking = false;
+                playerState.isAttacking = false;
+                currentFrame = idleAnimation.getKeyFrame(playerState.stateTime, true);
+                playerState.currentStateTime = 0f;
+            }
+        }
+        else if (playerState.isStrongKicking) {
+            if (!strongKickAnimation.isAnimationFinished(playerState.currentStateTime)) {
+                currentFrame = strongKickAnimation.getKeyFrame(playerState.currentStateTime, false);
+                playerState.currentStateTime += deltaTime;
+            }
+            else {
+                playerState.isStrongKicking = false;
+                playerState.isAttacking = false;
+                currentFrame = idleAnimation.getKeyFrame(playerState.stateTime, true);
+                playerState.currentStateTime = 0f;
             }
         }
         else {
-            currentFrame = idleAnimation.getKeyFrame(stateTime, true);
-            if (shouldPlayIdleAnimation) {
-                if (!autoTauntAnimation.isAnimationFinished(currentStateTime)) {
-                    currentFrame = autoTauntAnimation.getKeyFrame(currentStateTime, false);
-                    currentStateTime += deltaTime;
-                } else {
-                    shouldPlayIdleAnimation = false;
-                    currentFrame = idleAnimation.getKeyFrame(stateTime, true);
-                    currentStateTime = 0f;
+            currentFrame = idleAnimation.getKeyFrame(playerState.stateTime, true);
+            if (playerState.shouldPlayIdleAnimation) {
+                if (!autoTauntAnimation.isAnimationFinished(playerState.currentStateTime)) {
+                    currentFrame = autoTauntAnimation.getKeyFrame(playerState.currentStateTime, false);
+                    playerState.currentStateTime += deltaTime;
+                }
+                else {
+                    playerState.shouldPlayIdleAnimation = false;
+                    currentFrame = idleAnimation.getKeyFrame(playerState.stateTime, true);
+                    playerState.currentStateTime = 0f;
                 }
             }
         }
 
         sprite.setRegion(currentFrame);
-        sprite.setFlip(!isFacingRight, false);
-        if (isLightPunching) {
-            if (isFacingRight)
-                attackRectangle.set((position.x + 50 * scale), (sprite.getRegionHeight() - 35) * scale, 50 * scale, 20 * scale);
-            else
-                attackRectangle.set((position.x), (sprite.getRegionHeight() - 35) * scale, 50 * scale, 20 * scale);
-        } else attackRectangle.set(position.x, position.y, 0, 0);
-        rectangle.set((position.x + 14 * scale), position.y, 50 * scale, 106 * scale);
+        sprite.setFlip(!playerState.isFacingRight, false);
+
+        if (playerState.isAttacking) {
+            CharacterCollision.HitboxType hitboxType = CharacterCollision.HitboxType.LIGHT_PUNCH;
+            int currentFrameIndex = 0, startIndex = 0, endIndex = 0;
+            float x;
+            Hitbox[] hitboxes = new Hitbox[]{new Hitbox(0, 0, 0, 0)};
+
+            if (playerState.isLightPunching) {
+                currentFrameIndex = lightPunchAnimation.getKeyFrameIndex(playerState.currentStateTime);
+                startIndex = 1; endIndex = 2;
+                x = (playerState.isFacingRight) ? 80 : 16;
+                hitboxes = new Hitbox[]{
+                        new Hitbox(x, sprite.getRegionHeight() - 64, 45, 20),
+                        new Hitbox(x, sprite.getRegionHeight() - 64, 45, 20),
+                };
+            }
+            else if (playerState.isLightKicking) {
+                hitboxType = CharacterCollision.HitboxType.LIGHT_KICK;
+                currentFrameIndex = lightKickAnimation.getKeyFrameIndex(playerState.currentStateTime);
+                startIndex = 2; endIndex = 3;
+                x = (playerState.isFacingRight) ? 80 : 8;
+                hitboxes = new Hitbox[]{
+                        new Hitbox(x, sprite.getRegionHeight() - 105, 55, 40),
+                        new Hitbox(x, sprite.getRegionHeight() - 105, 55, 40),
+                };
+            }
+            else if (playerState.isStrongPunching) {
+                hitboxType = CharacterCollision.HitboxType.STRONG_PUNCH;
+                currentFrameIndex = strongPunchAnimation.getKeyFrameIndex(playerState.currentStateTime);
+                startIndex = 3; endIndex = 5;
+                x = (playerState.isFacingRight) ? 82 : 10;
+                hitboxes = new Hitbox[]{
+                        new Hitbox(x, sprite.getRegionHeight() - 74, 50, 20),
+                        new Hitbox(x, sprite.getRegionHeight() - 74, 50, 20),
+                        new Hitbox(x, sprite.getRegionHeight() - 74, 50, 20),
+                };
+            }
+            else if (playerState.isStrongKicking) {
+                hitboxType = CharacterCollision.HitboxType.STRONG_KICK;
+                currentFrameIndex = strongKickAnimation.getKeyFrameIndex(playerState.currentStateTime);
+                startIndex = 3; endIndex = 4;
+                x = (playerState.isFacingRight) ? 80 : 10;
+                hitboxes = new Hitbox[]{
+                        new Hitbox(x, sprite.getRegionHeight() - 66, 58, 36),
+                        new Hitbox(x, sprite.getRegionHeight() - 66, 58, 36),
+                };
+            }
+
+            collision.setHitboxesByType(hitboxType, hitboxes);
+            collision.iterateHitboxes(hitboxType, currentFrameIndex, startIndex, endIndex);
+        }
+
+        collision.setBodyRectangle(44, 0, 50, 106);
         characterAnimation.render(batch, position.x, position.y);
     }
 
