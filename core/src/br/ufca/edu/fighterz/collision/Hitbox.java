69bd545d2f273df522dd104d3c7fc4165b585dbd
Blackoutseeker <felipeoriginal.2019@gmail.com>
2024-03-31 21:18:06 -0300

    feat(collision): create Hitbox class

---
+++ core/src/br/ufca/edu/fighterz/collision/Hitbox.java
@@ -0,0 +1,15 @@
+package br.ufca.edu.fighterz.collision;
+
+public final class Hitbox {
+    final float x;
+    final float y;
+    final float width;
+    final float height;
+
+    public Hitbox(final float x, final float y, final float width, final float height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
