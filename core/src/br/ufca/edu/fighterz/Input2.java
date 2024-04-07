8fb1f24c569cd8b93340d00c29f6a498cd224110
Blackoutseeker <felipeoriginal.2019@gmail.com>
2024-04-07 03:25:56 -0300

    feat(input): create Input2 class

---
+++ core/src/br/ufca/edu/fighterz/Input2.java
@@ -0,0 +1,17 @@
+package br.ufca.edu.fighterz;
+
+import br.ufca.edu.fighterz.state.PlayerState;
+
+import com.badlogic.gdx.Input;
+
+public class Input2 extends InputHandler {
+    public Input2(final PlayableCharacter playableCharacter,
+                  final PlayerState playerState,
+                  final AudioManager audioManager) {
+        super(playableCharacter, playerState, audioManager,
+                new int[] {Input.Keys.LEFT, Input.Keys.RIGHT, Input.Keys.DOWN},
+                new int[] {Input.Keys.NUMPAD_5, Input.Keys.NUMPAD_4, Input.Keys.NUMPAD_6, Input.Keys.NUMPAD_8},
+                new int[] {Input.Keys.NUMPAD_1, Input.Keys.NUMPAD_2, Input.Keys.NUMPAD_3}
+        );
+    }
+}
