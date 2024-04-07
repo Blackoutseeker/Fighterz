6236e9530e6c86af928c6f7bb1b1077a1ca8c1c4
Blackoutseeker <felipeoriginal.2019@gmail.com>
2024-04-07 03:25:25 -0300

    refactor(input): simplify InputHandler methods

--- core/src/br/ufca/edu/fighterz/InputHandler.java
+++ core/src/br/ufca/edu/fighterz/InputHandler.java
@@ -1,98 +1,122 @@
 package br.ufca.edu.fighterz;
 
+import br.ufca.edu.fighterz.interfaces.KeyboardInputProcessor;
 import br.ufca.edu.fighterz.state.PlayerState;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 
-public final class InputHandler {
+abstract class InputHandler implements KeyboardInputProcessor {
+    private final PlayableCharacter playableCharacter;
     private final PlayerState playerState;
     private final AudioManager audioManager;
-    private final boolean isSecondPlayer;
+    private final int[] movementKeys;
+    private final int[] attackKeys;
+    private final int[] specialAttacksKeys;
 
-    public InputHandler(final PlayerState playerState, AudioManager audioManager, boolean isSecondPlayer) {
+    InputHandler(final PlayableCharacter playableCharacter,
+                 final PlayerState playerState, final AudioManager audioManager,
+                 final int[] movementKeys, final int[] attackKeys,
+                 final int[] specialAttacksKeys) {
+        this.playableCharacter = playableCharacter;
         this.playerState = playerState;
         this.audioManager = audioManager;
-        this.isSecondPlayer = isSecondPlayer;
+        this.movementKeys = movementKeys;
+        this.attackKeys = attackKeys;
+        this.specialAttacksKeys = specialAttacksKeys;
     }
 
-    public void handleInput(final float deltaTime) {
-        playerState.isPlayerInputLocked = Gdx.input.isKeyPressed(Input.Keys.S) && Gdx.input.isKeyPressed(Input.Keys.W);
-        if (!playerState.isPlayerInputLocked) {
-            if ((Gdx.input.isKeyPressed(Input.Keys.A) && !isSecondPlayer) || (Gdx.input.isKeyPressed(Input.Keys.LEFT) && isSecondPlayer) && !playerState.isAttacking) {
-                playerState.isWalkingLeft = true;
-                playerState.isWalkingRight = false;
-            } else if ((Gdx.input.isKeyPressed(Input.Keys.D) && !isSecondPlayer) || (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && isSecondPlayer) && !playerState.isAttacking) {
-                playerState.isWalkingLeft = false;
-                playerState.isWalkingRight = true;
-            } else {
-                playerState.isWalkingLeft = false;
-                playerState.isWalkingRight = false;
-            }
-            
+    private void handleMovementKeys() {
+        if (Gdx.input.isKeyPressed(movementKeys[0]) && !playerState.isAttacking) {
+            playerState.isWalkingLeft = true;
+            playerState.isWalkingRight = false;
+            resetAutoTauntAnimation();
+        }
+        else if (Gdx.input.isKeyPressed(movementKeys[1]) && !playerState.isAttacking) {
+            playerState.isWalkingLeft = false;
+            playerState.isWalkingRight = true;
+            resetAutoTauntAnimation();
+        }
+        else {
+            playerState.isWalkingLeft = false;
+            playerState.isWalkingRight = false;
+        }
+        if (Gdx.input.isKeyPressed(movementKeys[2]) && !playerState.isAttacking) {
+            playerState.isCrouching = true;
+            resetAutoTauntAnimation();
+        }
+        else playerState.isCrouching = false;
+    }
+
+    private void handleAttackKeys() {
+        if (Gdx.input.isKeyJustPressed(attackKeys[0])) {
+            audioManager.playCharacterSound(playableCharacter, 0);
+            playerState.isLightPunching = true;
+            playerState.isAttacking = true;
+            resetAutoTauntAnimation();
+        }
+        else if (Gdx.input.isKeyJustPressed(attackKeys[1])) {
+            audioManager.playCharacterSound(playableCharacter, 1);
+            playerState.isStrongPunching = true;
+            playerState.isAttacking = true;
+            resetAutoTauntAnimation();
+        }
+        else if (Gdx.input.isKeyJustPressed(attackKeys[2])) {
+            audioManager.playCharacterSound(playableCharacter, 2);
+            playerState.isLightKicking = true;
+            playerState.isAttacking = true;
+            resetAutoTauntAnimation();
+        }
+        else if (Gdx.input.isKeyJustPressed(attackKeys[3])) {
+            audioManager.playCharacterSound(playableCharacter, 3);
+            playerState.isStrongKicking = true;
+            playerState.isAttacking = true;
+            resetAutoTauntAnimation();
+        }
+    }
+
+    private void handleSpecialAttacksKeys() {
+        if (Gdx.input.isKeyJustPressed(specialAttacksKeys[0])) {
+            audioManager.playCharacterSound(playableCharacter,7);
+            playerState.isDoingSpecial0 = true;
+            playerState.isAttacking = true;
+            resetAutoTauntAnimation();
+        }
+        else if (Gdx.input.isKeyJustPressed(specialAttacksKeys[1])) {
+            audioManager.playCharacterSound(playableCharacter, 8);
+            playerState.isDoingSpecial1 = true;
+            playerState.isAttacking = true;
+            resetAutoTauntAnimation();
+        }
+        else if (Gdx.input.isKeyJustPressed(specialAttacksKeys[2])) {
+            audioManager.playCharacterSound(playableCharacter,9);
+            playerState.isDoingSpecial2 = true;
+            playerState.isAttacking = true;
+            resetAutoTauntAnimation();
+        }
+    }
+
+    @Override
+    public void handleInput(float deltaTime) {
+        if (!playerState.isPlayerInputLocked && !playerState.isGettingHit) {
+            handleMovementKeys();
             if (!playerState.isAttacking) {
-                if ((Gdx.input.isKeyJustPressed(Input.Keys.K) && !isSecondPlayer) || (Gdx.input.isKeyPressed(Input.Keys.NUMPAD_5) && isSecondPlayer)) {
-                    if(!isSecondPlayer)
-                        audioManager.playCharacterSound(0, 0);
-                    else
-                        audioManager.playCharacterSound(1,0);
-                    playerState.isLightPunching = true;
-                    playerState.isAttacking = true;
-                } else if ((Gdx.input.isKeyJustPressed(Input.Keys.J) && !isSecondPlayer) || (Gdx.input.isKeyPressed(Input.Keys.NUMPAD_4) && isSecondPlayer)) {
-                    if(!isSecondPlayer)
-                        audioManager.playCharacterSound(0, 1);
-                    else
-                        audioManager.playCharacterSound(1, 1);
-                    playerState.isStrongPunching = true;
-                    playerState.isAttacking = true;
-                } else if ((Gdx.input.isKeyJustPressed(Input.Keys.L) && !isSecondPlayer) || (Gdx.input.isKeyPressed(Input.Keys.NUMPAD_6) && isSecondPlayer)) {
-                    if(!isSecondPlayer)
-                        audioManager.playCharacterSound(0,2);
-                    else
-                        audioManager.playCharacterSound(1, 2);
-                    playerState.isLightKicking = true;
-                    playerState.isAttacking = true;
-                } else if ((Gdx.input.isKeyJustPressed(Input.Keys.I) && !isSecondPlayer) || (Gdx.input.isKeyPressed(Input.Keys.NUMPAD_8) && isSecondPlayer)) {
-                    if(!isSecondPlayer)
-                        audioManager.playCharacterSound(0,3);
-                    else
-                        audioManager.playCharacterSound(1, 3);
-                    playerState.isStrongKicking = true;
-                    playerState.isAttacking = true;
-                } else if ((Gdx.input.isKeyJustPressed(Input.Keys.U) && !isSecondPlayer) || (Gdx.input.isKeyPressed(Input.Keys.NUMPAD_1) && isSecondPlayer)) {
-                    if(!isSecondPlayer)
-                        audioManager.playCharacterSound(0,7);
-                    else
-                        audioManager.playCharacterSound(1,7);
-                    playerState.isDoingSpecial0 = true;
-                    playerState.isAttacking = true;
-                } else if ((Gdx.input.isKeyJustPressed(Input.Keys.O) && !isSecondPlayer) || (Gdx.input.isKeyPressed(Input.Keys.NUMPAD_2) && isSecondPlayer)) {
-                    if(isSecondPlayer)
-                        audioManager.playCharacterSound(0,8);
-                    else
-                        audioManager.playCharacterSound(1, 8);
-                    playerState.isDoingSpecial1 = true;
-                    playerState.isAttacking = true;
-                } else if ((Gdx.input.isKeyJustPressed(Input.Keys.P) && !isSecondPlayer) || (Gdx.input.isKeyPressed(Input.Keys.NUMPAD_3) && isSecondPlayer)) {
-                    if(!isSecondPlayer)
-                        audioManager.playCharacterSound(0,9);
-                    else
-                        audioManager.playCharacterSound(1,9);
-                    playerState.isDoingSpecial2 = true;
-                    playerState.isAttacking = true;
-                }
-            }
-
-
-                playerState.isCrouching = (Gdx.input.isKeyPressed(Input.Keys.S) &&!isSecondPlayer) || (Gdx.input.isKeyPressed(Input.Keys.DOWN) && isSecondPlayer) && !playerState.isAttacking;
-
-                if (!playerState.isWalkingLeft && !playerState.isWalkingRight
-                        && !playerState.isCrouching && !playerState.isAttacking) {
-                    playerState.idleTimer += deltaTime;
-                } else {
-                    playerState.idleTimer = 0f;
-                    playerState.shouldPlayIdleAnimation = false;
-                }
+                handleAttackKeys();
+                handleSpecialAttacksKeys();
             }
         }
+        if (!playerState.isWalkingLeft && !playerState.isWalkingRight
+                && !playerState.isCrouching && !playerState.isAttacking) {
+            playerState.idleTimer += deltaTime;
+        }
+        else {
+            resetAutoTauntAnimation();
+        }
+    }
+
+    @Override
+    public void resetAutoTauntAnimation() {
+        playerState.idleTimer = 0f;
+        playerState.autoTauntStateTime = 0f;
+        playerState.shouldPlayAutoTauntAnimation = false;
+    }
 }
