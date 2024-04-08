7c759c0f71702a1137923e4fe834012aa5d3b791
Blackoutseeker <felipeoriginal.2019@gmail.com>
2024-04-07 21:28:34 -0300

    feat(state): implements PlayerStateBehavior, create new two states and a new method

--- core/src/br/ufca/edu/fighterz/state/PlayerState.java
+++ core/src/br/ufca/edu/fighterz/state/PlayerState.java
@@ -1,6 +1,8 @@
 package br.ufca.edu.fighterz.state;
 
-public class PlayerState {
+import br.ufca.edu.fighterz.interfaces.PlayerStateBehavior;
+
+public final class PlayerState implements PlayerStateBehavior {
     public float stateTime = 0f;
     public float idleTimer = 0f;
     public float currentStateTime = 0f;
@@ -11,9 +13,9 @@ public class PlayerState {
     public boolean isCrouching = false;
     public boolean isFacingRight = true;
     public boolean isLightPunching = false;
-    public  boolean isStrongPunching = false;
-    public  boolean isLightKicking = false;
-    public  boolean isStrongKicking = false;
+    public boolean isStrongPunching = false;
+    public boolean isLightKicking = false;
+    public boolean isStrongKicking = false;
     public boolean isAttacking = false;
     public boolean isGettingHit = false;
     public boolean isBlocking = false;
@@ -22,4 +24,33 @@ public class PlayerState {
     public boolean isDoingSpecial1 = false;
     public boolean isDoingSpecial2 = false;
     public float life = 200;
+    public boolean isDefeated = false;
+    public boolean isWinning = false;
+
+    @Override
+    public void resetAllStates() {
+        stateTime = 0f;
+        idleTimer = 0f;
+        currentStateTime = 0f;
+        autoTauntStateTime = 0f;
+        shouldPlayAutoTauntAnimation = false;
+        isWalkingLeft = false;
+        isWalkingRight = false;
+        isCrouching = false;
+        isFacingRight = true;
+        isLightPunching = false;
+        isStrongPunching = false;
+        isLightKicking = false;
+        isStrongKicking = false;
+        isAttacking = false;
+        isGettingHit = false;
+        isBlocking = false;
+        isPlayerInputLocked = false;
+        isDoingSpecial0 = false;
+        isDoingSpecial1 = false;
+        isDoingSpecial2 = false;
+        life = 200;
+        isDefeated = false;
+        isWinning = false;
+    }
 }
